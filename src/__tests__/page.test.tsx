6aaf9ac78454e52f81075e3a47df7a0958ad0655
Dhruv Gupta <dhruv035@gmail.com>
2025-01-09 03:49:15 +0530

    cleanupp

--- src/__tests__/page.test.tsx
+++ src/__tests__/page.test.tsx
@@ -12,42 +12,40 @@ import VaultCard from "../components/VaultCard/VaultCard";
 jest.mock("@starknet-react/core", () => ({
   __esModule: true,
   useNetwork: jest.fn(),
-  useProvider: jest.fn(),
-  useReadContract: jest.fn(),
-  useAccount:jest.fn(),
 }));
 
-jest.mock("../components/VaultCard/VaultCard", ()=>({
-  __esModule:true,
-  default:jest.fn(({ vaultAddress }: { vaultAddress: string }) => (
-    <div data-testid={`mocked-child-${vaultAddress}`}>Mocked Child {vaultAddress}</div>
-  ))
-}))
-jest.mock("next/navigation", () => ({
-  useRouter: jest.fn(),
-  usePathname: jest.fn(),
+jest.mock("../components/VaultCard/VaultCard", () => ({
+  __esModule: true,
+  default: jest.fn(({ vaultAddress }: { vaultAddress: string }) => (
+    <div data-testid={`mocked-child-${vaultAddress}`}>
+      Mocked Child {vaultAddress}
+    </div>
+  )),
 }));
+
 jest.mock("../hooks/websocket/useWebSocketHome", () => ({
   __esModule: true,
   default: jest.fn(),
 }));
 
-jest.mock ("../hooks/vault/useVaultState",()=>({
-  __esModule:true,
-  default:jest.fn()
-}))
-jest.mock ("../hooks/optionRound/state/useRoundState",()=>({
-  __esModule:true,
-  default:jest.fn()
-}))
 jest.mock("../hooks/window/useIsMobile", () => ({
   __esModule: true, // This makes sure Jest treats it as a module with exports
   default: jest.fn(), // Mock the default export
 }));
-jest.mock("../hooks/window/useIsMobile", () => ({
-  __esModule: true, // This makes sure Jest treats it as a module with exports
-  default: jest.fn(), // Mock the default export
-}));
+
+type MockHookParams = {
+  isMobile?:boolean,
+  network?:string,
+  vaults?:string[]
+}
+const mockHooks = ({isMobile,network,vaults}:MockHookParams) => {
+  (useIsMobile as jest.Mock).mockReturnValue({ isMobile:isMobile??false });
+  (useNetwork as jest.Mock).mockReturnValue({
+    chain: { network:network??"testnet" },
+  });
+  (useWebSocketHome as jest.Mock).mockReturnValue({ vaults:vaults??[""]});
+};
+
 
 describe("Home Component", () => {
   beforeEach(() => {
@@ -55,35 +53,20 @@ describe("Home Component", () => {
   });
 
   it("renders the MobileScreen component when isMobile is true", () => {
-    (useIsMobile as jest.Mock).mockReturnValue({ isMobile: false });
-    (useNetwork as jest.Mock).mockReturnValue({
-      chain: { network: "testnet" },
-    });
-    (useWebSocketHome as jest.Mock).mockReturnValue({ vaults: [""] });
-
-    (useRouter as jest.Mock).mockReturnValue({
-      pathname: "/home",
-      push: jest.fn(),
+    mockHooks({
+      isMobile:true,
     });
 
-    (useProvider as jest.Mock)
-      .mockReturnValue({
-        provider: {
-          getBlock: () => "1",
-          getChainId: () => "1",
-        },
-      });
-      (useReadContract as jest.Mock)
-      .mockReturnValue({
-        data: {},
-      });
-      (useAccount as jest.Mock)
-      .mockReturnValue({
-        account: {},
-      });
-  
-
-   
+    render(<Home />);
+    screen.debug();
+    
+    //Update to checks for MobileScreen
+    expect(screen.getByText(/device not supported/i)).toBeInTheDocument();
+  });
+  it("renders the default screen component when isMobile is true", () => {
+    mockHooks({
+      isMobile:false,
+    });
     render(<Home />);
     screen.debug();
     expect(screen.getByText(/popular/i)).toBeInTheDocument();
