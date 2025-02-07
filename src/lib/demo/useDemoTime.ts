9fc25c784a818c8df44dc215f71a3164911dff04
Matt Carter <96356887+0xDegenDeveloper@users.noreply.github.com>
2025-02-07 10:13:42 -0500

    fix demoTime hook to build

--- src/lib/demo/useDemoTime.ts
+++ src/lib/demo/useDemoTime.ts
@@ -1,17 +1,24 @@
 import { useState, useEffect } from "react";
 
-export const useDemoTime = (active: boolean, intervalMS = 15000): number => {
+export const useDemoTime = (
+  active: boolean,
+  isEnabled: boolean,
+  intervalMS = 15000,
+): number => {
   const [demoNow, setDemoNow] = useState<number>(
-    () => new Date().getTime() / 1000,
+    isEnabled ? new Date().getTime() / 1000 : 0,
   );
 
   useEffect(() => {
-    if (!active) return;
+    if (!active || !isEnabled) return;
+
     const interval = setInterval(() => {
       setDemoNow(new Date().getTime() / 1000);
     }, intervalMS);
-    return () => clearInterval(interval);
-  }, [active, intervalMS]);
 
-  return Number(demoNow.toFixed(0));
+    return () => clearInterval(interval);
+  }, [active, isEnabled, intervalMS]);
+
+  // Ensure a valid return value
+  return isEnabled ? Number(demoNow?.toFixed(0)) : 0;
 };
