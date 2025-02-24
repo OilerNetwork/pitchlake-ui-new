a2b00fb24301298298a7e8e2cf9a2427259c74f6
Matt Carter <96356887+0xDegenDeveloper@users.noreply.github.com>
2025-02-24 00:39:10 -0500

    Update ProfileDropdown.tsx

--- src/components/BaseComponents/ProfileDropdown.tsx
+++ src/components/BaseComponents/ProfileDropdown.tsx
@@ -37,47 +37,51 @@ const ProfileDropdown: React.FC<ProfileDropdownProps> = ({
         </span>
         <CopyIcon className="h-4 w-4 text-[var(--buttongrey)] cursor-pointer" />
       </Hoverable>
-      <div className="px-4 py-2 text-sm text-[var(--buttonwhite)] border-b border-greyscale-800">
-        <Hoverable dataId="accountDropdownBalanceHeader">
-          <h3 className="text-[var(--buttongrey)] mb-3 mt-2 text-[12px]">
-            MY BALANCE
-          </h3>
-        </Hoverable>
-        <div className="space-y-3">
-          <Hoverable
-            dataId="accountDropdownBalance"
-            className="flex justify-between font-regular"
-          >
-            <span>Wallet</span>
-            <span>{balance.wallet} ETH</span>
-          </Hoverable>
-          {pathName?.includes("vault") && (
-            <>
-              <Hoverable
-                dataId="accountDropdownLocked"
-                className="flex justify-between font-regular"
-              >
-                <span>Locked</span>
-                <span>{balance.locked} ETH</span>
-              </Hoverable>
-              <Hoverable
-                dataId="accountDropdownUnlocked"
-                className="flex justify-between font-regular"
-              >
-                <span>Unlocked</span>
-                <span>{balance.unlocked} ETH</span>
-              </Hoverable>
-              <Hoverable
-                dataId="accountDropdownStashed"
-                className="flex justify-between font-regular"
-              >
-                <span>Stashed</span>
-                <span>{balance.stashed} ETH</span>
-              </Hoverable>
-            </>
-          )}
-        </div>
-      </div>
+
+      {
+        //<div className="px-4 py-2 text-sm text-[var(--buttonwhite)] border-b border-greyscale-800">
+        //  <Hoverable dataId="accountDropdownBalanceHeader">
+        //    <h3 className="text-[var(--buttongrey)] mb-3 mt-2 text-[12px]">
+        //      WALLET
+        //    </h3>
+        //  </Hoverable>
+        //<div className="space-y-3">
+        //       <Hoverable
+        //         dataId="accountDropdownBalance"
+        //         className="flex justify-between font-regular"
+        //       >
+        //         <span>ETH Balance</span>
+        //         <span>{balance.wallet} ETH</span>
+        //       </Hoverable>
+        //    {pathName?.includes("vault") && (
+        //      <>
+        //        <Hoverable
+        //          dataId="accountDropdownLocked"
+        //          className="flex justify-between font-regular"
+        //        >
+        //          <span>Locked</span>
+        //          <span>{balance.locked} ETH</span>
+        //        </Hoverable>
+        //        <Hoverable
+        //          dataId="accountDropdownUnlocked"
+        //          className="flex justify-between font-regular"
+        //        >
+        //          <span>Unlocked</span>
+        //          <span>{balance.unlocked} ETH</span>
+        //        </Hoverable>
+        //        <Hoverable
+        //          dataId="accountDropdownStashed"
+        //          className="flex justify-between font-regular"
+        //        >
+        //          <span>Stashed</span>
+        //          <span>{balance.stashed} ETH</span>
+        //        </Hoverable>
+        //      </>
+        //    )}
+        //</div>
+        //</div>
+      }
+
       <div
         className="px-4 py-3 text-sm text-white hover:bg-greyscale-800 cursor-pointer flex justify-between items-center font-regular"
         onClick={disconnect}
